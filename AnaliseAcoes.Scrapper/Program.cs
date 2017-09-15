0cf3247261c95fed7f1762b52f61b6cc414b5db4
Douglas Everton de Oliveira <douglas.oliveira@antt.gov.br>
2017-09-15 16:49:22 -0300

    Adding scrapper project

---
+++ AnaliseAcoes.Scrapper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnaliseAcoes.Scrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
